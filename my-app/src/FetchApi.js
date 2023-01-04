9279d0f4e78296cc5954b585e3fa5bf07bf6ef88
Andrews'sSavagery <40343154+andrewscoding2018@users.noreply.github.com>
2023-01-03 16:11:00 -0800

    Update FetchAPI.js

--- my-app/src/FetchApi.js
+++ my-app/src/FetchApi.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function FetchAPI() {
   const [data, setData] = useState([]);
 
-
-const Stock = ({ time, USD, EUR, GBP }) => {
+  const Stock = ({ time, USD, EUR, GBP }) => {
     return (
       <table>
         <tbody>
@@ -12,7 +11,7 @@ const Stock = ({ time, USD, EUR, GBP }) => {
             <td>
               <h5>{time}</h5>
             </td>
-            <td>
+            {/* <td>
               <h5>{USD}</h5>
             </td>
             <td>
@@ -20,38 +19,48 @@ const Stock = ({ time, USD, EUR, GBP }) => {
             </td>
             <td>
               <p>{GBP}</p>
-            </td>
+            </td> */}
           </tr>
         </tbody>
       </table>
     );
   };
-  
-  const apiGet = () => {
-    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then((response) => response.json())
-      .then((json) =>{
-          console.log(json);
-          setData(json);
-      })
-      
-    
-    };
+
+  const apiGet = async () => {
+    const res = await fetch(
+      "https://api.coindesk.com/v1/bpi/currentprice.json"
+    );
+    const data1 = await res.json();
+    setData(data1);
+    console.log(data1);
+  };
+
+  const displayData = () => {
+    return (
+      <div>
+        <p>{data.disclaimer}</p>
+        <p>{data.bpi.USD.code}</p>
+      </div>
+    );
+  };
+
   return (
     <>
       <button onClick={apiGet}>Fetch API</button>
+      <>{displayData}</>
       <div className="stock-container">
-        {data.map((data, key) => {
+        {/* {data.map((item, i) => {
           return (
-            <div key={key}>
+            <div key={i}>
               <Stock
-              time = {data.time}
-              USD = {data.USD}
-              EUR = {data.EUR}
-              GBP = {data.GBP}/>
+              time = {item.time}
+            //   USD = {data.USD}
+            //   EUR = {data.EUR}
+            //   GBP = {data.GBP}
+              />
             </div>
           );
-        })}
+        })} */}
       </div>
     </>
   );
